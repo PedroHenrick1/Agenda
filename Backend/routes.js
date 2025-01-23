b5767cb836568ce0cb453ab5766c2a7562954b70
PedroHenrick1 <pedrohenrick52@gmail.com>
2025-01-23 11:26:50 -0300

    rodando-server

---
+++ Backend/routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const route = express.Router();
+
+const usersController = require('./controllers/usersControllers');
+
+route.get('/', usersController.paginaInicial);
+
